a40ed37c9d0e206becb238913074c59f5b99b7fa
“Afarisphi” <agung.afr10@gmail.com>
2023-09-21 15:26:14 +0700

    b

---
+++ app.js
@@ -0,0 +1,13 @@
+// // process.env
+// const server = new Server({
+//   host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
+// });
+
+// // process.memoryUsage()
+// const cpuInformation = process.memoryUsage();
+// console.log(cpuInformation);
+
+// process.argv
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+console.log(`Hello ${firstName} ${lastName}`);
---
+++ get_user.js
@@ -0,0 +1,8 @@
+/**
+* Gunakan object destructuring untuk mengimpor lebih dari satu nilai pada modul.
+*/
+const { firstName, lastName } = require('./user');
+
+
+console.log(firstName);
+console.log(lastName);
